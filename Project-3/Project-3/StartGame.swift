80dcd41ae48649c1f7ace3121a4eea36eb33ee7a
juliek03 <59373281+juliek03@users.noreply.github.com>
2020-05-11 16:49:35 +0200

    Update StartGame.swift

--- Project-3/Project-3/StartGame.swift
+++ Project-3/Project-3/StartGame.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+class Game {
+    var player1: Player?
+    var player2: Player?
+    
+    func newGame() {
+        player1 = createPlayer()
+        player2 = createPlayer()
+        
+        playGame()
+        displayStat()
+    }
+    
+    func createPlayer() -> Player { //création du joueur
+       //demande au joueur d'entrer le nom du perso
+        let playerName = "Magic"
+        let player = Player(name: playerName)
+                
+        while !player.isTeamFull() {
+            //le joueur sélectionne un joueur et lui donne un nom
+            player.addCharacter(character: createCharacter())
+        }
+        return player
+    }
+    
+    func createCharacter() -> Characters {
+        let character = Magician(name: "Magic")
+        return character //le joueur doit sélectionner un perso
+    }
+    
+    func playGame() {
+        while !player1!.hasLost() || !player2!.hasLost() {
+            //les joueurs jouent au tour par tour jusqu'au perdant
+        }
+    }
+    
+    func displayStat() {
+        //les stats du jeu
+        player1?.displayStats()
+        player2?.displayStats()
+    }
+}
+//les joueurs ajoutent des personnages, check de la team si elle perd sinon c'est au tour du joueur 2
